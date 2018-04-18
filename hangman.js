7d80c454689414a6273cca814515981f641be332
lesya <lesyaolexs852@gmail.com>
2018-04-18 18:48:12 +0300

    change logic

--- hangman.js
+++ hangman.js
@@ -5,7 +5,8 @@ var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 
 var wordsArray = ['cappuccino', 'dog', 'present', 'fanny', 'street', 'head'];
 var word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
-var leaves = 6;
+var overAttempt = 6;
+var attempt = overAttempt;
 
 
 //create buttons for alphabet letters
@@ -13,6 +14,7 @@ function buttons() {
     for (i = 0; i < alphabet.length; i++) {
         var buttons_letters = document.createElement('button');
         buttons_letters.appendChild(document.createTextNode(alphabet[i]));
+        buttons_letters.setAttribute('onclick','guess(this)');
         document.getElementById("buttons_alphabet").appendChild(buttons_letters);
     }
 
@@ -30,34 +32,42 @@ function guessWord() {
 
 //open guessed letters, changes the image to fail
 
-function guesses(t) {
-    var guess_letter = t.innerHTML;
-    for (i = 0; i < word.length; i++ ){
-        if (word[i] === guess_letter){
+function guess(buttonElement) {
+
+    var guess_letter = buttonElement.innerHTML;
+
+    buttonElement.disabled = true;
+
+    for (i = 0; i < word.length; i++) {
+        if (word[i] === guess_letter) {
             document.getElementById('guess_word').children[i].innerHTML = guess_letter;
-
-
-
         }
     }
-    for (j = 0; j < document.getElementById('guess_word').children.length; j++){
-        if(document.getElementById('guess_word').children[j].innerHTML !== '_'){
-            document.getElementById('game_over').innerHTML = 'Winner!';
-        }
-        else {
-            leaves-- ;
-            document.getElementById('images_hangman').src= 'hangman_image/part'+ (6-leaves) + '.jpg';
-            if (leaves === 0){
+
+    if (document.getElementById('guess_word').innerHTML.indexOf(guess_letter) === -1) {
+                attempt--;
+                document.getElementById('images_hangman').src = 'hangman_image/part' + (overAttempt - attempt) + '.jpg';
+                document.getElementById('game_over').innerHTML = 'Try again!';
+    }
+
+
+    if (attempt===0){
                 document.getElementById('game_over').innerHTML = 'Loser';
-            }
-
-        }
-        t.disabled = true
     }
-
-
+    else if ('_' in document.getElementById('guess_word').innerHTML === false){
+            document.getElementById('game_over').innerHTML = 'Win!';
+    }
 
 }
+
+//create restart button
+
+var buttonRestart = document.createElement('button');
+buttonRestart.document.getElementById('buttonRestart');
+
+
+
+
 
 
 
